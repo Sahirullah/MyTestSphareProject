3dd3148d945ed3d67afeb12e424475e407b7e10f
Sahirullah <sahirullah313@gmail.com>
2026-08-21 13:05:15 +0500

    updating

--- src/data/examPracticeData.js
+++ src/data/examPracticeData.js
@@ -589,4 +589,5 @@ export const quizCategories = [
   { id: 'airforce', name: 'AIR FORCE - Air Force'},
   { id: 'army', name: 'ARMY - army'},
   { id: 'ielts', name: 'IELTS - English Language Test'},
+  { id: 'ppsc', name: 'PPSC - Punjab Public Service Commission'},
 ];
