b554cbcb8e02f41f34a82ff941daac657ae7cb6d
Sahirullah <sahirullah313@gmail.com>
2026-08-22 10:36:02 +0500

    updating exam

--- src/data/examPracticeData.js
+++ src/data/examPracticeData.js
@@ -671,7 +671,7 @@ export const examPracticeData = [
   {"question": "Q.97: Hollywood is famous for which type of work?", "options": ["Statue Making", "Paintings", "Film Making", "Trade and Development"], "correct": 2}
 ]},
 
-//PPSC deputy accountant free mock test 02
+//PPSC deputy accountant free mock test 02 
   { id: 95, code: 'PPSC', title: 'PPSC punjab public service commission deputy accountant free mock test 02', category: 'ppsc', image: '👮', questions: [
   {"question": "Q.1: A fjord refers to?", "options": ["Storm in ocean", "long, narrow sea inlet characterized by steep cliffs or mountains", "Whirl in sand dune", "Rock in river"], "correct": 1},
   {"question": "Q.2: Pakistan total area is?", "options": ["796096 Sq.km", "696096 Sq.km", "896096 Sq.km", "996096 Sq.km"], "correct": 0},
