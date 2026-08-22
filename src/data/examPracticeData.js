a21ff3d5a4342aa49c4af6dba7c489caad031330
Sahirullah <sahirullah313@gmail.com>
2026-08-22 10:23:43 +0500

    updating exam

--- src/data/examPracticeData.js
+++ src/data/examPracticeData.js
@@ -570,8 +570,8 @@ export const examPracticeData = [
     {"question": "PMA stands for?", "options": ["Pakistan Military Academy", "Public Military Authority", "Pakistan Armed Movement", "Personnel Military Affairs"], "correct": 0},
     {"question": "What is the main objective of PMA?", "options": ["Military training", "Officer commission", "Research", "Engineering"], "correct": 1},
   ]},
-//PPSC free mock test 01
-  { id: 95, code: 'PPSC', title: 'PPSC punjab public service commission free mock test 01', category: 'ppsc', image: '👮', questions: [
+//PPSC deputy accountant free mock test 01
+  { id: 95, code: 'PPSC', title: 'PPSC punjab public service commission deputy accountant free mock test 01', category: 'ppsc', image: '👮', questions: [
   {"question": "Q.1: A fjord refers to?", "options": ["Storm in ocean", "long, narrow sea inlet characterized by steep cliffs or mountains", "Whirl in sand dune", "Rock in river"], "correct": 1},
   {"question": "Q.2: Pakistan total area is?", "options": ["796096 Sq.km", "696096 Sq.km", "896096 Sq.km", "996096 Sq.km"], "correct": 0},
   {"question": "Q.3: Internal Digital phenomena include Volcanism, tech tonic plates and ____?", "options": ["Convergent", "Civergent", "Diastrophism", "Denudation"], "correct": 2},
