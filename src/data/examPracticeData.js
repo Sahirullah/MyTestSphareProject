621e4791f3670e77f9e03ab2ba9ed1a405caf348
Sahirullah <sahirullah313@gmail.com>
2026-08-22 10:38:52 +0500

    updating exam

--- src/data/examPracticeData.js
+++ src/data/examPracticeData.js
@@ -777,7 +777,7 @@ export const examPracticeData = [
 
   ]},
 
-//PPSC Assistant free mock test 01 
+//PPSC Assistant free mock test 02
   { id: 95, code: 'PPSC', title: 'PPSC punjab public service commission Assistant free mock test 02', category: 'ppsc', image: '👮', questions: [
 
   ]},
