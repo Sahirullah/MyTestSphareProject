8046407ea47662cd5fa7568e7da754836fc3f35f
Sahirullah <sahirullah313@gmail.com>
2026-08-24 12:51:37 +0500

    updating

--- src/data/examPracticeData.js
+++ src/data/examPracticeData.js
@@ -811,7 +811,7 @@ export const examPracticeData = [
 
   ]},
 
-//PPSC Assistant free mock test 05
+//Class Assistant free mock test 05
   { id: 95, code: '9th10th', title: 'Class 9th free mock physics test practice', category: 'ppsc', image: '👮', questions: [
 
   ]},
