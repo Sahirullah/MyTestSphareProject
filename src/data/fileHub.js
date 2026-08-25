2c502ecbffb3e1ae2927f00ac916930283f88311
Sahirullah <sahirullah313@gmail.com>
2026-08-25 11:48:48 +0500

    linking some books

--- src/data/fileHub.js
+++ src/data/fileHub.js
@@ -780,21 +780,21 @@ export const finalTermDataPart3 = [
 
   // FIA - Fedral Investigatoin Agency
   // FIA - ASI
-  { id: 128, code: 'ASI', title: 'FIA ASI all important pdf files download free', category: 'fia', image: '🕵️‍♂️', links: [
+  { id: 128, code: 'FIA', title: 'FIA ASI all important pdf files download free', category: 'fia', image: '🕵️‍♂️', links: [
     'https://drive.google.com/file/d/1FQeLJRFZRbAHDzT9PmConrBDtvg_KPgj/view?usp=sharing',
     'https://drive.google.com/file/d/1ErTN-8r8zs2ym8TrI9ioXbDHaU39lMl7/view?usp=sharing',
     'https://drive.google.com/file/d/1LMDCa-IMolTEP13O1uyBR9JeDSR9xuJS/view?usp=sharing'
   ]},
 
   // FIA - Assistant sub inspector
-  { id: 128, code: 'ASI', title: 'FIA ASI all important pdf files download free', category: 'fia', image: '🕵️‍♂️', links: [
+  { id: 128, code: 'FIA', title: 'FIA ASI all important pdf files download free', category: 'fia', image: '🕵️‍♂️', links: [
     'https://drive.google.com/file/d/1XF558pwLryrm0WVLQhgPtocQbbubVEO-/view?usp=sharing',
     'https://drive.google.com/file/d/1Ae3b9aslI_pWnPFMkVl1FlrEi3XCrkOm/view?usp=sharing',
     'https://drive.google.com/file/d/1RTCIMserPWWH_eki5Kn7ptwsgKMEmxD6/view?usp=sharing'
   ]},
 
   // FIA - LDC
-  { id: 128, code: 'ASI', title: 'FIA ASI all important pdf files download free', category: 'fia', image: '🕵️‍♂️', links: [
+  { id: 128, code: 'FIA', title: 'FIA ASI all important pdf files download free', category: 'fia', image: '🕵️‍♂️', links: [
     'https://drive.google.com/file/d/1RHkklaJcQ4J4hZQQYHysWT-RNt6fRy9T/view?usp=sharing',
     'https://drive.google.com/file/d/1RHkklaJcQ4J4hZQQYHysWT-RNt6fRy9T/view?usp=sharing'
   ]},
