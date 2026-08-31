1c6618a8e605ae6cf5f10dbe27cb871e7b84595b
Sahirullah <sahirullah313@gmail.com>
2026-08-31 14:04:49 +0500

    updating

--- src/data/fileHub.js
+++ src/data/fileHub.js
@@ -671,6 +671,7 @@ export const finalTermDataPart3 = [
   { id: 108, code: 'ECO601', title: 'Business Econometrics', category: 'eco', image: '📊' },
 //ECO606
   { id: 109, code: 'ECO606', title: 'Mathematical Economics I', category: 'eco', image: '📊' },
+// 
   { id: 110, code: 'ECO607', title: 'Mathematical Economics II', category: 'eco', image: '📊' },
   { id: 111, code: 'ECO612', title: 'Population Economics', category: 'eco', image: '📊' },
   
