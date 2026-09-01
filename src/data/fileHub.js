5409ab7fe11a714d1dab61f7eda790e22cd50bfe
Sahirullah <sahirullah313@gmail.com>
2026-09-01 13:09:42 +0500

    updating

--- src/data/fileHub.js
+++ src/data/fileHub.js
@@ -17,6 +17,7 @@ export const finalTermData = [
     'https://docs.google.com/document/d/1hjLw6Jzw3rOb5vpVxlU4YO3coPrnpt1a/edit?usp=sharing&ouid=114958120571553419825&rtpof=true&sd=true',
     'https://drive.google.com/file/d/1RAuP9xX8XrXutkqq0C3r6TouTYlBQ8dy/view?usp=sharing'
   ]},
+  
   { id: 5, code: 'BIF402', title: 'Ethical & Legal Issues in Bioinformatics', category: 'bif', image: '🧬',links:[
     'https://docs.google.com/document/d/1CD44mp3vjH6Et-2Rd2ewGyBoY-o7FS-i/edit?usp=sharing&ouid=114958120571553419825&rtpof=true&sd=true'
   ]},
