ebce5a618fe99085f48bb3a5226a6861fb53ba2f
Sahirullah <sahirullah313@gmail.com>
2026-09-05 08:14:11 +0500

    updating

--- src/data/fileHub.js
+++ src/data/fileHub.js
@@ -427,6 +427,7 @@ export const finalTermDataPart2 = [
     'https://docs.google.com/document/d/1Kvsomkte-A6ONIOBVsCTAnQP14jErdXq/edit?usp=sharing&ouid=114958120571553419825&rtpof=true&sd=true',
     'https://drive.google.com/uc?export=download&id=1EJEmyUpW96pJcq5A9uh2eDL-A9jWA82R'
   ] },
+  //
   { id: 73, code: 'CS504', title: 'Software Engineering I', category: 'cs', image: '💻',links:[
     'https://drive.google.com/uc?export=download&id=14VhyJ7iDsFh7XUE6xrRJ5F7FzSFX2o0G',
     'https://drive.google.com/uc?export=download&id=114CRaF4ui925VclXxhnO1LUZFBqJ0lWk',
