f8ad3701599c07b6d0d4496fc6ad6120c89ddc02
Sahirullah <sahirullah313@gmail.com>
2026-09-09 14:10:08 +0500

    now i am starting to add Fedral text board books

--- src/data/schoolBaseData.js
+++ src/data/schoolBaseData.js
@@ -112,7 +112,7 @@ export const schoolBaseData = [
   { id: 23, code: 'BOARD008', title: 'BISE Gujranwala', category: 'boards', image: '📚', link: '' },
   //      >>>>------<<<<<       HYDERABAD TEXTBOOK BOARD       >>>>------<<<<<
   { id: 24, code: 'BOARD009', title: 'BISE Hyderabad', category: 'boards', image: '📚', link: '' },
-  //      >>>>------<<<<<       DERA GHAZI KHAN TEXTBOOK BOARD       >>>>------<<<<<
+  //      >>>>------<<<<<       KARACHI TEXTBOOK BOARD       >>>>------<<<<<
   { id: 25, code: 'BOARD010', title: 'BISE Karachi', category: 'boards', image: '📚', link: '' },
   { id: 26, code: 'BOARD011', title: 'BISE Khuzdar', category: 'boards', image: '📚', link: '' },
   { id: 27, code: 'BOARD012', title: 'BISE Kohat', category: 'boards', image: '📚', link: '' },
