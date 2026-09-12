86d2571335041463c44d71aa673164b133dc8eec
Sahirullah <sahirullah313@gmail.com>
2026-09-12 16:37:17 +0500

    Fix broken imports: remove non-existent data exports and fix react-router imports

--- src/utils/searchUtils.js
+++ src/utils/searchUtils.js
@@ -1,6 +1,6 @@
 import { assignmentData, assignmentDataClass10, allAssignmentData } from '../data/assignmentData';
-import { midtermData, allMidtermData } from '../data/pastPaper';
-import { finalTermData, allFinalTermData } from '../data/fileHub';
+import { midtermData } from '../data/pastPaper';
+import { finalTermData } from '../data/fileHub';
 import { allBooksData } from '../data/allBooksData';
 import { examPracticeData } from '../data/examPracticeData';
 
