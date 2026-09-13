e7fe2e0ea52dfcd4caca853c1851160bbf8a6727
Sahirullah <sahirullah313@gmail.com>
2026-09-13 13:38:36 +0500

    updating

--- src/data/uniBaseData.js
+++ src/data/uniBaseData.js
@@ -365,7 +365,11 @@ export const uniBaseDataPart2 = [
     'https://drive.google.com/file/d/1LrnrLWo7FD2rDsWZ9MmG7xOYzI8AYnQD/view?usp=sharing',
     'https://drive.google.com/file/d/1ZPtFTlToSQoO8Mono7Bl5F3uy2WufGZb/view?usp=sharing'
   ] },
-  { id: 137, code: 'MATH202', title: 'HRM 628', category: 'hrm', image: '👩‍💼', links: [] },
+  { id: 137, code: 'MATH401', title: 'math401 .midterm past papers.', category: 'hrm', image: '👩‍💼', links: [
+    'https://drive.google.com/file/d/1uvBNKEr0XzQrwVduhsoL7ZWCxAgZtPL5/view?usp=sharing',
+    'https://drive.google.com/file/d/1kWApmlXTVR4xwpQKP06IpplYvEuRI2Co/view?usp=sharing',
+    'https://drive.google.com/file/d/1d-tfVGwAqLvtIWHhxAa_TdF-ODb7as0D/view?usp=sharing'
+  ] },
   { id: 138, code: 'MATH202', title: 'HRM 628', category: 'hrm', image: '👩‍💼', links: [] },
   { id: 139, code: 'MATH202', title: 'HRM 628', category: 'hrm', image: '👩‍💼', links: [] },
  // { id: 139, code: 'MATH202', title: 'HRM 628', category: 'hrm', image: '👩‍💼', links: [] },
